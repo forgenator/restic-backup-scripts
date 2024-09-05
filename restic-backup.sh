ca22b96a569bb86e5a89e6e95809330763ea7d8b
forgenator <forgenator@users.noreply.github.com>
2024-09-05 10:36:48 +0300

    Update restic-backup.sh

--- restic-backup.sh
+++ restic-backup.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
+# Call this script like this
+# resctic-backup.sh "tag" "directory_to_backup"
+
+# Run cron every midnight
+# 0 0 * * * /path/to/restic-backup.sh "tag" "directory_to_backup"
+
 # Static configuration
 RESTIC=/usr/bin/restic
-CONFIG_DIR=/home/$USER/.config/restic/
+CONFIG=/home/$USER/.config/restic/env.conf
 LOGFILE=/home/$USER/restic.log
+TAG=$1
+BACKUP_DIRECTORY=$2
 
 # Get the environment also in this file, in case it's not read properly
-. $CONFIG/env.conf
+. $CONFIG
 
 # Gotify notification
 notify()
@@ -15,26 +23,25 @@ notify()
                 -F "title=${1}" \
                 -F "message=${2}" \
                 -F "priority=5" \
-                "${Gotify_URL}/message?token=${Gotify_Token}"
+                "${GOTIFY_URL}/message?token=${GOTIFY_TOKEN}"
 
 }
 
 # Run backup
 if $RESTIC backup \
-            --tag $1 \ # Tag to apply to the backup
-            $2 \ # Directory to backup
-            > /dev/null 2>&1; 
+            --tag $TAG \ # Tag to apply to the backup
+            $BACKUP_DIRECTORY \ # Directory to backup
+            >> $LOGFILE 2>&1; 
 then
   echo "$TIMESTAMP Restic backup succesfull" >> $LOGFILE
-  notify "Restic backup succesfull" "Succesfull backup of $2" 
+  notify "Restic backup succesfull" "Succesfull backup of $BACKUP_DIRECTORY" 
 else
   echo "$TIMESTAMP Restic backup went wrong" >> $LOGFILE
-  notify "Restic backup broken" "Backup of $2 didn't went trough." 
+  notify "Restic backup broken" "Backup of $BACKUP_DIRECTORY didn't went trough." 
 fi
           
 
 # Remove snapshots according to policy
-# If run cron more frequently, might add --keep-hourly 24
 $RESTIC forget \
             --keep-daily 7 \
             --keep-weekly 4 \
@@ -46,5 +53,3 @@ $RESTIC prune
 
 # Check the repository for errors
 $RESTIC check
-
-# Done
